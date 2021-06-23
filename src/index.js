adea73ee38c16f63e727ec6c63438de040c99984
tangweikun <819573105@qq.com>
2021-06-23 21:22:21 +0800

    add multiple entry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
@@ -8,6 +9,11 @@ function component() {
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   var myIcon = new Image();
   myIcon.src = Icon;
