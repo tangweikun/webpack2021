318176f638cdee2ea82e05aeb87aede18401baaa
tangweikun <819573105@qq.com>
2021-06-24 13:39:18 +0800

    add clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   // entry: './src/index.js',
@@ -20,6 +21,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management',
     }),
