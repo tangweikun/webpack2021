fc2c988f7f774f9d4db5616dd82a086b2bd936fd
tangweikun <819573105@qq.com>
2021-06-24 11:30:38 +0800

    add html-webpack-plugin

--- src/print.js
+++ src/print.js
@@ -1,3 +1,5 @@
 export default function printMe() {
+  const foo = 5;
+  console.log(foo);
   console.log('I get called from print.js!');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // entry: './src/index.js',
   entry: {
     app: './src/index.js',
     print: './src/print.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -21,5 +18,14 @@ module.exports = {
         use: ['file-loader'],
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
